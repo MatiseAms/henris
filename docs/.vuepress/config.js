8269dc456ab7dab88691f55595de22d809e43319
Stephan Hoogland <stephan@shoogland.com>
2018-06-12 11:46:55 +0200

    fixed all the menu sidbar things

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,9 @@
+let getMenu = function() {
+	let menu = ["/", "/scss/"];
+	// let menu = ['scss/typography','scss/color'];
+	return menu;
+};
+
 module.exports = {
 	title: "Henri's",
 	description: "Just playing around",
@@ -6,6 +12,10 @@ module.exports = {
 		editLinks: true,
 		docsDir: 'docs',
 		lastUpdated: 'Last Updated',
-		editLinkText: 'Edit this page on GitHub'
+		editLinkText: 'Edit this page on GitHub',
+		nav: [
+			{ text: 'Matise.nl', link: 'https://www.matise.nl' },
+		],
+		sidebar: getMenu()
 	}
 }
