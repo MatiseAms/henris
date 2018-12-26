2e6c693a1938c7fc7ce9a7b05038c939749d6c60
Dennis Dipsaus <dennis.spierenburg@hotmail.com>
2018-12-26 17:27:11 +0100

    working progress not finished yet, compiling data now

--- scripts/css-functions.js
+++ scripts/css-functions.js
@@ -1,93 +1,78 @@
 let convert = require("color-convert");
+module.exports = functions = {
+  assets(data, v) {
+    let theValue = null;
+    let vArray = v.split(".");
+    if (vArray.length > 1) {
+      if (vArray.length == 3) {
+        if (data[vArray[0]][vArray[1]][vArray[2]]) {
+          theValue = data[vArray[0]][vArray[1]][vArray[2]];
+        } else {
+          theValue = data[vArray[0]][vArray[1]][0][vArray[2]];
+        }
+      }
+    }
+    if (theValue === null) {
+      theValue = v;
+    }
+    return theValue;
+  },
+  color(data, v) {
+    let theColor;
+    let searchColor = v;
+    let transparency = 1;
 
-let functions = {};
+    let vArray = v.split(",");
+    if (vArray.length > 1) {
+      searchColor = vArray[0];
+      transparency = vArray[1];
+    }
 
-functions.isNumber = n => {
-	return !isNaN(parseFloat(n)) && isFinite(n);
+    Object.keys(data.color).forEach(function(list) {
+      if (list !== "title") {
+        data.color[list].forEach(function(colorList) {
+          Object.keys(colorList).map(function(value, key) {
+            if (value.toLowerCase() === searchColor.toLowerCase()) {
+              theColor = colorList[value];
+              if (transparency < 1) {
+                theColor =
+                  "rgba(" +
+                  convert.hex.rgb(theColor.substring(1)).join(",") +
+                  "," +
+                  transparency +
+                  ")";
+              }
+            }
+          });
+        });
+      }
+    });
+    return theColor;
+  },
+  isNumber(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  },
+  grid(data, v) {
+    function gridCalc(g) {
+      let grid = 100 / data.grid.columns;
+      return grid * g + "vw";
+    }
+    if (functions.isNumber(v)) {
+      return gridCalc(v);
+    } else {
+      if (v.split(" ").length > 1) {
+        let multi = [];
+        v.split(" ").forEach(function(val) {
+          if (functions.isNumber(val)) {
+            multi.push(gridCalc(val));
+          } else {
+            console.log(val, " - Nan");
+          }
+        });
+        return multi.join(" ");
+      } else {
+        return v;
+      }
+    }
+  }
 };
-
-/*
- The function to generate grid based values
-*/
-functions.grid = function(data, v) {
-	function gridCalc(g) {
-		let grid = 100 / data.grid.columns;
-		return grid * g + "vw";
-	}
-	if (functions.isNumber(v)) {
-		return gridCalc(v);
-	} else {
-		if (v.split(" ").length > 1) {
-			let multi = [];
-			v.split(" ").forEach(function(val) {
-				if (functions.isNumber(val)) {
-					multi.push(gridCalc(val));
-				} else {
-					console.log(val, " - Nan");
-				}
-			});
-			return multi.join(" ");
-		} else {
-			return v;
-		}
-	}
-};
-
-/*
- Return colors from the colors
-*/
-functions.color = (data, v) => {
-	let theColor;
-	let searchColor = v;
-	let transparency = 1;
-
-	let vArray = v.split(",");
-	if (vArray.length > 1) {
-		searchColor = vArray[0];
-		transparency = vArray[1];
-	}
-	
-	Object.keys(data.color).forEach(function(list) {
-		if (list !== "title") {
-			data.color[list].forEach(function(colorList) {
-				Object.keys(colorList).map(function(value, key) {
-					if (value.toLowerCase() === searchColor.toLowerCase()) {
-						theColor = colorList[value];
-						if (transparency < 1) {
-							theColor =
-								"rgba(" +
-								convert.hex.rgb(theColor.substring(1)).join(",") +
-								"," +
-								transparency +
-								")";
-						}
-					}
-				});
-			});
-		}
-	});
-	return theColor;
-};
-
-/*
- Return assets setting from settings
-*/
-functions.assets = (data, v) => {
-	let theValue = null;
-	let vArray = v.split(".");
-	if (vArray.length > 1) {
-		if (vArray.length == 3) {
-			if (data[vArray[0]][vArray[1]][vArray[2]]) {
-				theValue = data[vArray[0]][vArray[1]][vArray[2]];
-			} else {
-				theValue = data[vArray[0]][vArray[1]][0][vArray[2]];
-			}
-		}
-	}
-	if (theValue === null) {
-		theValue = v;
-	}
-	return theValue;
-};
-
-module.exports = functions;
--- scripts/json-to-css.js
+++ scripts/json-to-css.js
@@ -1,63 +1,52 @@
-const fs = require('fs');
-const path = require('path');
-const flatten = require('flat');
-const chalk = require('chalk');
-const functions = require('./css-functions.js');
-const Table = require('cli-table');
+const fs = require("fs");
+const path = require("path");
+const flatten = require("flat");
+const chalk = require("chalk");
+const helperFunction = require("./css-functions.js");
+const Table = require("cli-table");
 
-const delimiter = '-',
-	sourceFolder = 'data/',
-	distFolder = 'src/scss/';
+const delimiter = "-",
+  sourceFolder = "data/",
+  distFolder = "src/scss/";
 
 let fileTypes = [
-	{
-		type: 'scss',
-		dest: distFolder + 'settings',
-		varPattern: '${{var}}: {{value}} !default; //',
-		listPatternParent: '${{var}}: (\n{{list}}\n) !default; //',
-		listSubPatternParent: '\t"{{var}}": (\n{{list}}\n\t) //',
-		listPattern: '\t"{{var}}": {{value}}'
-	}
+  {
+    type: "scss",
+    dest: distFolder + "settings",
+    varPattern: "${{var}}: {{value}} !default; //",
+    listPatternParent: "${{var}}: (\n{{list}}\n) !default; //",
+    listSubPatternParent: '\t"{{var}}": (\n{{list}}\n\t) //',
+    listPattern: '\t"{{var}}": {{value}}'
+  }
 ];
 
 String.prototype.replaceAll = function(search, replacement) {
-	var target = this;
-	return target.split(search).join(replacement);
+  var target = this;
+  return target.split(search).join(replacement);
 };
-// css option
-// {
-// 	type: 'css',
-// 	dest: distFolder + 'css/settings',
-// 	varPattern: '--{{var}}: {{value}};',
-// 	listPatternParent: '',
-// 	listPattern: ''
-// }
 
 /**
  * Get all setting files from /data
  */
 function getFiles(dir, files_) {
-	files_ = files_ || [];
-	var files = fs.readdirSync(dir);
-	for (var i in files) {
-		if (files[i].split('.')[files[i].split('.').length - 1] === 'json') {
-			//	console.log(files[i]);
-			var name = dir + '/' + files[i];
-			if (fs.statSync(name).isDirectory()) {
-				getFiles(name, files_);
-			} else {
-				files_.push(name);
-			}
-		}
-	}
-	//	console.log(files_);
-	return files_;
+  files_ = files_ || [];
+  var files = fs.readdirSync(dir);
+  for (var i in files) {
+    if (files[i].split(".")[files[i].split(".").length - 1] === "json") {
+      //	console.log(files[i]);
+      var name = dir + "/" + files[i];
+      if (fs.statSync(name).isDirectory()) {
+        getFiles(name, files_);
+      } else {
+        files_.push(name);
+      }
+    }
+  }
+  return files_;
 }
 
-
-
 function isObject(item) {
-  return (item && typeof item === 'object' && !Array.isArray(item));
+  return item && typeof item === "object" && !Array.isArray(item);
 }
 
 function mergeDeep(target, ...sources) {
@@ -77,250 +66,306 @@ function mergeDeep(target, ...sources) {
   return mergeDeep(target, ...sources);
 }
 
-
-// Store the used data before compiling;
-let stored = {};
-getFiles(sourceFolder).forEach((file) => {
-	stored = mergeDeep({},stored,JSON.parse(fs.readFileSync(file, 'utf8')));
-});
-
-function removeKeys(value) {
-	return value.replace('-0-', '-');
-}
-
-// Do functions when necessary;
+// Do helperFunction when necessary;
 function doFunction(value) {
-	let thefunc = value.replace('{{', '').replace('}}', '');
-	let func = thefunc.split('(')[0];
-	let parameters = value.replace(/(^.*\(|\).*$)/g, '');
-	let newvalue;
-	if (typeof functions[func] === 'function') {
-		newvalue = functions[func](stored, parameters);
-	} else {
-		newvalue = func + '(' + parameters + ')';
-	}
-	return newvalue;
+  let thefunc = value.replace("{{", "").replace("}}", "");
+  let func = thefunc.split("(")[0];
+  let parameters = value.replace(/(^.*\(|\).*$)/g, "");
+  let newvalue;
+  if (typeof helperFunction[func] === "function") {
+    newvalue = helperFunction[func](stored, parameters);
+  } else {
+    newvalue = func + "(" + parameters + ")";
+  }
+  return newvalue;
 }
 
-// Do functions when necessary;
+// Do helperFunction when necessary;
 function makeList(value) {
-	let newvalue = value.replace('[[', '').replace(']]', '');
-	return newvalue;
+  let newvalue = value.replace("[[", "").replace("]]", "");
+  return newvalue;
 }
 
 function stringValue(value) {
-	if (typeof value == 'string') {
-		let quotes = true;
-		let noquotes = ['normal', 'regular', 'lowercase', 'uppercase', 'none', '0'];
-		if (
-			value.indexOf('px') > -1 ||
-			value.indexOf('ch') > -1 ||
-			value.indexOf('rem') > -1 ||
-			value.indexOf('%') > -1
-		) {
-			quotes = false;
-		}
-		if (value.indexOf('(') > -1 && value.indexOf(')') > -1) {
-			quotes = false;
-		}
-		if (value.charAt(0) === '#') {
-			quotes = false;
-		}
-		if (noquotes.includes(value)) {
-			quotes = false;
-		}
-		if (parseFloat(value)) {
-			quotes = false;
-		}
-		// Do Functions
-		if (value.indexOf('{{') > -1) {
-			value = stringValue(doFunction(value));
-		}
-		// makeList
-		if (value) {
-			if (value.indexOf('[[') > -1) {
-				value = makeList(value);
-			}
-		}
-		if (quotes) {
-			return "'" + value + "'";
-		} else {
-			return value;
-		}
-	} else {
-		return value;
-	}
+  if (typeof value == "string") {
+    let quotes = true;
+    let noquotes = ["normal", "regular", "lowercase", "uppercase", "none", "0"];
+    if (
+      value.indexOf("px") > -1 ||
+      value.indexOf("ch") > -1 ||
+      value.indexOf("rem") > -1 ||
+      value.indexOf("%") > -1
+    ) {
+      quotes = false;
+    }
+    if (value.indexOf("(") > -1 && value.indexOf(")") > -1) {
+      quotes = false;
+    }
+    if (value.charAt(0) === "#") {
+      quotes = false;
+    }
+    if (noquotes.includes(value)) {
+      quotes = false;
+    }
+    if (parseFloat(value)) {
+      quotes = false;
+    }
+    // Do helperFunction
+    if (value.indexOf("{{") > -1) {
+      value = stringValue(doFunction(value));
+    }
+    // makeList
+    if (value) {
+      if (value.indexOf("[[") > -1) {
+        value = makeList(value);
+      }
+    }
+    if (quotes) {
+      return "'" + value + "'";
+    } else {
+      return value;
+    }
+  } else {
+    return value;
+  }
 }
-
-function objToStyle(file, type) {
-	let variable;
-	let newFile = [];
-
-	let data = flatten(file, {
-		delimiter: delimiter
-	});
-	let listData = flatten(file, {
-		delimiter: delimiter,
-		safe: true
-	});
-
-	// Do the variables
-	Object.keys(data).forEach((key) => {
-		if (!functions.isNumber(key.split('-')[key.split('-').length - 1])) {
-			variable = type.varPattern
-				.replace('{{var}}', removeKeys(key.toLowerCase()))
-				.replace('{{value}}', stringValue(data[key]));
-			newFile.push(removeKeys(variable));
-		}
-	});
-
-	// Do the lists
-	function stringify(object, iteration, inObjectList = false) {
-		let list = [];
-
-		Object.keys(object).forEach((key) => {
-			if (typeof object[key][0] === 'object') {
-				if (iteration > 0) {
-					list.push(
-						type.listSubPatternParent
-							.replace('{{var}}', removeKeys(key.toLowerCase()))
-							.replace(
-								'{{list}}',
-								stringify(object[key][0], iteration + 1, true)
-							)
-					);
-				} else {
-					list.push(
-						type.listPatternParent
-							.replace('{{var}}', removeKeys(key.toLowerCase()))
-							.replace(
-								'{{list}}',
-								stringify(object[key][0], iteration + 1, true)
-							)
-					);
-				}
-			} else if (
-				Object.prototype.toString.call(object[key]) === '[object Array]'
-			) {
-				list.push(
-					type.listPatternParent
-						.replace('{{var}}', removeKeys(key.toLowerCase()))
-						.replace('{{list}}', '"' + object[key].join('", \r\n"') + '"')
-				);
-			} else if (inObjectList) {
-				list.push(
-					type.listPattern
-						.replace('{{var}}', key)
-						.replace('{{value}}', stringValue(object[key]))
-				);
-			}
-		});
-		return list.join(', \r\n');
-	}
-
-	newFile.push(stringify(listData, 0));
-
-	return newFile.join('\r\n');
-}
-
-/**
- * Define the folder and get the files
- */
-function makeDirs() {
-	fileTypes.forEach((type) => {
-		const sep = path.sep;
-		const initDir = path.isAbsolute(type.dest) ? sep : '';
-		type.dest.split(sep).reduce((parentDir, childDir) => {
-			const curDir = path.resolve(parentDir, childDir);
-			if (!fs.existsSync(curDir)) {
-				fs.mkdirSync(curDir);
-			}
-			return curDir;
-		}, initDir);
-	});
-}
-makeDirs();
-
-function createImport(files) {
-	let code = '';
-	let codes = [];
-	files.forEach(function(file) {
-		code = code + '@import "' + file + '";\n';
-		codes.push(
-			'\t' +
-				chalk.hex('#7329b1').bold('@import ') +
-				"'" +
-				chalk.green(file) +
-				"';\n"
-		);
-	});
-	console.log('\n\n\t' + chalk.yellow.bold('Create @imports') + '\n');
-
-	console.log(codes.join(''));
-
-	fileTypes.forEach((type) => {
-		fs.writeFileSync(type.dest + '/_index.' + type.type, code, function(err) {
-			console.log('woops, something went wrong!' + err);
-		});
-	});
-}
-
 /**
  * Convert all files
  */
 
-let settingFiles = [];
-let i = 0;
-var table = new Table({
-	head: [
-		chalk.hex('#7329b1').bold('File'),
-		chalk.hex('#7329b1').bold('Input'),
-		chalk.hex('#7329b1').bold('To'),
-		chalk.hex('#7329b1').bold('Output')
-	],
-	colWidths: [20, 25, 10, 25]
+let stored = {};
+getFiles(sourceFolder).forEach(file => {
+  stored = mergeDeep({}, stored, JSON.parse(fs.readFileSync(file, "utf8")));
 });
-console.log('\n\n\t' + chalk.yellow.bold('Convert json data to Sass') + '\n');
-console.log(table.toString());
-getFiles(sourceFolder).forEach((file) => {
-	i++;
 
-	let fileName = file
-		.split('/')
-		[file.split('/').length - 1].replace('.json', '');
-	if (fileName.charAt(0) !== '_') {
-		fileTypes.forEach((type) => {
-			// Write New Files
-			let compiled = objToStyle(
-				JSON.parse(fs.readFileSync(file, 'utf8')),
-				type
-			);
+class JSONToCSS {
+  constructor() {
+    this.setBaseSettings();
+    this.setFolder();
+    this.setBaseFiles(this.settings.sourceFolder);
 
-			// Fix for less lists;
-			compiled = compiled.replaceAll(';,', ';');
-			var table = new Table({
-				head: false,
-				colWidths: [20, 25, 10, 25]
-			});
-			table.push([
-				chalk.green.bold(fileName),
-				file.split('/')[file.split('/').length - 1],
-				chalk.green('\u2192 ' + type.type),
-				fileName + '.' + type.type
-			]);
-			console.log(table.toString());
+    //if init is done, go fill the files
+    this.setAllFiles();
+  }
+  setBaseSettings() {
+    this.settings = {};
+    this.settings.delimiter = "-";
+    this.settings.sourceFolder = "data/";
+    this.settings.distFolder = "src/scss/";
+    this.settings.fileTypes = [
+      {
+        type: "scss",
+        dest: distFolder + "settings",
+        varPattern: "${{var}}: {{value}} !default; //",
+        listPatternParent: "${{var}}: (\n{{list}}\n) !default; //",
+        listSubPatternParent: '\t"{{var}}": (\n{{list}}\n\t) //',
+        listPattern: '\t"{{var}}": {{value}}'
+      }
+    ];
+  }
 
-			fs.writeFileSync(
-				type.dest + '/_' + fileName + '.' + type.type,
-				compiled,
-				function(err) {
-					console.log('woops, something went wrong!' + err);
-				}
-			);
-		});
-		settingFiles.push(fileName);
-		if (i === getFiles(sourceFolder).length) {
-			createImport(settingFiles);
-		}
-	}
-});
+  setFolder() {
+    const fileTypes = this.getFileTypes();
+
+    //for each options of fileTypes execute some tasks
+    fileTypes.forEach(type => {
+      const sep = path.sep;
+      const initDir = path.isAbsolute(type.dest) ? sep : "";
+      type.dest.split(sep).reduce((parentDir, childDir) => {
+        const curDir = path.resolve(parentDir, childDir);
+        if (!fs.existsSync(curDir)) fs.mkdirSync(curDir);
+        return curDir;
+      }, initDir);
+    });
+  }
+  setBaseFiles(dir, files_) {
+    files_ = files_ || [];
+    var files = fs.readdirSync(dir);
+    for (var i in files) {
+      if (files[i].split(".")[files[i].split(".").length - 1] === "json") {
+        var name = dir + "/" + files[i];
+        if (fs.statSync(name).isDirectory()) {
+          this.setBaseFiles(name, files_);
+        } else {
+          files_.push(name);
+        }
+      }
+    }
+    this.baseFiles = files_;
+  }
+  setAllFiles() {
+    //log top of table
+    const table = new Table({
+      head: [
+        chalk.hex("#7329b1").bold("File"),
+        chalk.hex("#7329b1").bold("Input"),
+        chalk.hex("#7329b1").bold("To"),
+        chalk.hex("#7329b1").bold("Output")
+      ],
+      colWidths: [20, 25, 10, 25]
+    });
+    //execute first log
+    console.log(
+      "\n\n\t" + chalk.yellow.bold("Convert json data to Sass") + "\n"
+    );
+    console.log(table.toString());
+
+    //foreach filtypes
+    let settingFiles = [];
+    this.baseFiles.forEach((file, index) => {
+      //filename is created by split on / and get the last element of it
+      let fileName = file
+        .split("/")
+        [file.split("/").length - 1].replace(".json", "");
+
+      //filename cant start with _
+      if (fileName.charAt(0) !== "_") {
+        //for each setting we want to create a file
+        this.settings.fileTypes.forEach(type => {
+          // Write New Files based on file and type
+          let compiled = this.objToStyle({
+            file: JSON.parse(fs.readFileSync(file, "utf8")),
+            type
+          });
+          // Fix for less lists;
+          compiled = compiled.replaceAll(";,", ";");
+
+          //log output to console: file, input, to, output
+          this.logNewTable({
+            file: chalk.green.bold(fileName),
+            input: file.split("/")[file.split("/").length - 1],
+            to: chalk.green("\u2192 " + type.type),
+            output: fileName + "." + type.type
+          });
+
+          fs.writeFileSync(
+            type.dest + "/_" + fileName + "." + type.type,
+            compiled,
+            err => {
+              console.log("woops, something went wrong!" + err);
+            }
+          );
+        });
+
+        settingFiles.push(fileName);
+        //if on the end of loop createImport
+        if (index + 1 === getFiles(sourceFolder).length) {
+          this.setImport(settingFiles);
+        }
+      }
+    });
+  }
+  setImport(files) {
+    let code = "";
+    const codes = files.map(file => {
+      code = code + '@import "' + file + '";\n';
+      return (
+        "\t" +
+        chalk.hex("#7329b1").bold("@import ") +
+        "'" +
+        chalk.green(file) +
+        "';\n"
+      );
+    });
+
+    console.log("\n\n\t" + chalk.yellow.bold("Create @imports") + "\n");
+    console.log(codes.join(""));
+    fileTypes.forEach(type => {
+      fs.writeFileSync(type.dest + "/_index." + type.type, code, function(err) {
+        console.log("woops, something went wrong!" + err);
+      });
+    });
+  }
+  getFileTypes() {
+    return this.settings.fileTypes;
+  }
+  logNewTable({ file, input, to, output } = {}) {
+    console.log(file);
+    let table = new Table({
+      head: false,
+      colWidths: [20, 25, 10, 25]
+    });
+
+    table.push([file, input, to, output]);
+
+    console.log(table.toString());
+  }
+  objToStyle({ file, type } = {}) {
+    const self = this;
+    let variable;
+    let newFile = [];
+
+    let data = flatten(file, {
+      delimiter: delimiter
+    });
+    let listData = flatten(file, {
+      delimiter: delimiter,
+      safe: true
+    });
+
+    // Do the variables
+    Object.keys(data).forEach(key => {
+      //check if the key of the data is not a number
+      if (!helperFunction.isNumber(key.split("-")[key.split("-").length - 1])) {
+        variable = type.varPattern
+          .replace("{{var}}", self.removeKeys(key.toLowerCase()))
+          .replace("{{value}}", stringValue(data[key]));
+        newFile.push(self.removeKeys(variable));
+      }
+    });
+
+    // Do the lists
+    function stringify(object, iteration, inObjectList = false) {
+      let list = [];
+      Object.keys(object).forEach(key => {
+        if (typeof object[key][0] === "object") {
+          if (iteration > 0) {
+            list.push(
+              type.listSubPatternParent
+                .replace("{{var}}", self.removeKeys(key.toLowerCase()))
+                .replace(
+                  "{{list}}",
+                  stringify(object[key][0], iteration + 1, true)
+                )
+            );
+          } else {
+            list.push(
+              type.listPatternParent
+                .replace("{{var}}", self.removeKeys(key.toLowerCase()))
+                .replace(
+                  "{{list}}",
+                  stringify(object[key][0], iteration + 1, true)
+                )
+            );
+          }
+        } else if (
+          Object.prototype.toString.call(object[key]) === "[object Array]"
+        ) {
+          list.push(
+            type.listPatternParent
+              .replace("{{var}}", self.removeKeys(key.toLowerCase()))
+              .replace("{{list}}", '"' + object[key].join('", \r\n"') + '"')
+          );
+        } else if (inObjectList) {
+          list.push(
+            type.listPattern
+              .replace("{{var}}", key)
+              .replace("{{value}}", stringValue(object[key]))
+          );
+        }
+      });
+      return list.join(", \r\n");
+    }
+
+    newFile.push(stringify(listData, 0));
+
+    return newFile.join("\r\n");
+  }
+  removeKeys(value) {
+    return value.replace("-0-", "-");
+  }
+}
+
+const JSONToCSSInstance = new JSONToCSS();
+
+//do something
